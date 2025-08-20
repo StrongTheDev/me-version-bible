01fdee9e4818203c9883a05cba61bafbad770b08
DanielHuey <adhueywilan@gmail.com>
2025-08-20 12:39:38 +0300

    Remove VerseCard widget implementation from home.dart to streamline code structure

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:me_version_bible/models/selection.dart';
 import 'package:me_version_bible/pages/components/custom_drawer.dart';
 import 'package:me_version_bible/pages/components/custom_list_tile.dart';
+import 'package:me_version_bible/pages/components/verse_card.dart';
 import 'package:me_version_bible/providers/bible_provider.dart';
 import 'package:me_version_bible/utils/functions.dart';
 import 'package:provider/provider.dart';
@@ -396,62 +397,6 @@ class _HomeState extends State<Home> {
     _scrollChapter.jumpTo(selection.chapterIndex * bookAndChapterItemHeight);
 
     _initialScrollCompleted = true;
-  }
-}
-
-// ignore: must_be_immutable
-class VerseCard extends StatelessWidget {
-  VerseCard({
-    super.key,
-    required this.provider,
-    required this.verse,
-    this.color,
-    this.verseName,
-    this.opacity = 10,
-  });
-
-  final BibleProvider provider;
-  final Map<String, dynamic> verse;
-  final Color? color;
-  final String? verseName;
-
-  /// Values from 0-255
-  int opacity;
-
-  @override
-  Widget build(BuildContext context) {
-    opacity = opacity.clamp(0, 255);
-    return Container(
-      padding: EdgeInsets.all(12),
-      margin: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-      decoration: BoxDecoration(
-        color:
-            color ?? Theme.of(context).colorScheme.primary.withAlpha(opacity),
-        borderRadius: BorderRadius.circular(4),
-      ),
-      child: RichText(
-        text: TextSpan(
-          text:
-              verseName ??
-              "${provider.books.firstWhere((b) => b['id'] == verse['book_id'])['name']} ${verse['chapter']}:${verse['verse']}",
-          style: TextStyle(
-            color: Theme.of(context).colorScheme.primary,
-            fontWeight: FontWeight.bold,
-            fontSize: 16,
-            fontFamily: "Merriweather",
-          ),
-          children: [
-            TextSpan(
-              text: "   ${verse['text']}",
-              style: TextStyle(
-                color: Colors.white,
-                fontWeight: FontWeight.normal,
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
   }
 }
 
